10f9084ca4287bb33147d82e48172fb84cd60b9f
Soni Kumari <soni12032000soni@gmail.com>
2022-09-26 17:16:37 +0530

    Added files

---
+++ contact.php
@@ -0,0 +1,9 @@
+<h1 style="color:#000000"><b><center>Contact Details</center></b></h1>
+<div align="center">
+<p><b>Address :</b> Darbhanga College of Engineering, Near Mabbi, Darbhanga, Bihar (846005).
+<p><b>Phone :</b> 06272 255 255<br>
+<b>Mobile :</b> +91 797993532
+</p>
+<p><b>Email :</b> dcedbg@rediffmail.com</p>
+</p>
+</div>
